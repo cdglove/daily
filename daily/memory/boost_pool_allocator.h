b6cce32d53e12ce0c7b2ee88039566d0c402de29
Chris Glover <c.d.glover@gmail.com>
2014-12-01 21:59:46 -0500

    Fix typo with template params

--- daily/memory/boost_pool_allocator.h
+++ daily/memory/boost_pool_allocator.h
@@ -69,10 +69,10 @@ private:
 	friend class boost_pool_allocator;
 
 	template <typename U, typename V>
-	friend bool operator==(boost_pool_allocator<U> const& a, boost_pool_allocator<U> const& b);
+	friend bool operator==(boost_pool_allocator<U> const& a, boost_pool_allocator<V> const& b);
 
 	template <typename U, typename V>
-	friend bool operator!=(boost_pool_allocator<V> const& a, boost_pool_allocator<V> const& b);
+	friend bool operator!=(boost_pool_allocator<U> const& a, boost_pool_allocator<V> const& b);
 
 	boost::pool<UserAllocator>& pool_;
 };
@@ -80,14 +80,14 @@ private:
 
 // ----------------------------------------------------------------------------
 //
-template <class T, class U>
-bool operator==(boost_pool_allocator<T> const& a, boost_pool_allocator<U> const& b)
+template <typename U, typename V>
+bool operator==(boost_pool_allocator<U> const& a, boost_pool_allocator<V> const& b)
 {
 	return &a.pool_ == &b.pool_;
 }
 
-template <class T, class U>
-bool operator!=(boost_pool_allocator<T> const& a, boost_pool_allocator<U> const& b)
+template <typename U, typename V>
+bool operator==(boost_pool_allocator<U> const& a, boost_pool_allocator<V> const& b)
 {
 	return &a.pool_ != &b.pool_;
 }
